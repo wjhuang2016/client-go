74b18b5a4c1f0fd375e34ca513b2203c437554d0
wjhuang2016 <huangwenjun1997@gmail.com>
2021-12-13 16:20:17 +0800

    add
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- txnkv/transaction/2pc.go
+++ txnkv/transaction/2pc.go
@@ -1212,8 +1212,10 @@ func (c *twoPhaseCommitter) execute(ctx context.Context) (err error) {
 		// Plus 1 to avoid producing the same commit TS with previously committed transactions
 		c.minCommitTS = latestTS + 1
 	}
-	logutil.Logger(ctx).Error("before get prewrite 5 sec")
-	time.Sleep(5 * time.Second)
+	if c.sessionID != 0 {
+		logutil.Logger(ctx).Error("before get prewrite 5 sec")
+		time.Sleep(5 * time.Second)
+	}
 	// Calculate maxCommitTS if necessary
 	if commitTSMayBeCalculated {
 		if err = c.calculateMaxCommitTS(ctx); err != nil {
